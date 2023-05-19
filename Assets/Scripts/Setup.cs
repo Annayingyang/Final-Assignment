27b437dff314b8a836b60502bb9f4367ad94ea5d
NightLocke15 <132197652+2615283@users.noreply.github.com>
2023-05-19 09:16:43 +0200

    19/5/23

--- Assets/Scripts/Setup.cs
+++ Assets/Scripts/Setup.cs
@@ -2,68 +2,76 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Setup : GameObjects
+public class Setup : MonoBehaviour
 {
     private GameObject adveturerButton;
     private GameObject treasureCardButton;
-    // Start is called before the first frame update
+    private GameObject initialFloodCardButton;
+
+    GameObjects gameobjectsScript;
+
+    private void Awake()
+    {
+        gameobjectsScript = GameObject.Find("Game Board").GetComponent<GameObjects>();
+    }
+
     void Start()
     {
-        AddTilesToList();
-        PlaceTilesOnBoard();
-        AddTreasureList();
-        PlaceTreasureOnBoard();
-        AddAdventurersToList();
-        AddCardsToTreasureCards();
+        gameobjectsScript.AddTilesToList();
+        gameobjectsScript.PlaceTilesOnBoard();
+        gameobjectsScript.AddTreasureList();
+        gameobjectsScript.PlaceTreasureOnBoard();
+        gameobjectsScript.AddAdventurersToList();
+        gameobjectsScript.AddCardsToTreasureCards();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void DealAdventurerCards()
     {
         Vector3 apos1 = new Vector3(-6f, 3f, 0);
-        GameObject Adventurer1 = adventurers[Random.Range(0, 6)];
+        GameObject Adventurer1 = gameobjectsScript.adventurers[Random.Range(0, 6)];
         Adventurer1.transform.position = apos1;
-        adventurers.Remove(Adventurer1);
+        gameobjectsScript.adventurers.Remove(Adventurer1);
 
         Vector3 apos2 = new Vector3(6f, 3f, 0);
-        GameObject Adventurer2 = adventurers[Random.Range(0, 5)];
+        GameObject Adventurer2 = gameobjectsScript.adventurers[Random.Range(0, 5)];
         Adventurer2.transform.position = apos2;
-        adventurers.Remove(Adventurer2);
+        gameobjectsScript.adventurers.Remove(Adventurer2);
     }
 
     public void DealFirstTwoTreasureCardsAtStart()
     {
         Vector3 tcpos1 = new Vector3(-7.1f, 1.9f, 0);
-        GameObject TreasureCard1 = treasureCards[Random.Range(0, 24)];
+        GameObject TreasureCard1 = gameobjectsScript.treasureCards[Random.Range(0, 24)];
         TreasureCard1.transform.position = tcpos1;
-        treasureCards.Remove(TreasureCard1);
+        gameobjectsScript.treasureCards.Remove(TreasureCard1);
 
         Vector3 tcpos2 = new Vector3(-6f, 1.9f, 0);
-        GameObject TreasureCard2 = treasureCards[Random.Range(0, 23)];
+        GameObject TreasureCard2 = gameobjectsScript.treasureCards[Random.Range(0, 23)];
         TreasureCard2.transform.position = tcpos2;
-        treasureCards.Remove(TreasureCard2);
+        gameobjectsScript.treasureCards.Remove(TreasureCard2);
 
         Vector3 tcpos3 = new Vector3(7.1f, 1.9f, 0);
-        GameObject TreasureCard3 = treasureCards[Random.Range(0, 22)];
+        GameObject TreasureCard3 = gameobjectsScript.treasureCards[Random.Range(0, 22)];
         TreasureCard3.transform.position = tcpos3;
-        treasureCards.Remove(TreasureCard3);
+        gameobjectsScript.treasureCards.Remove(TreasureCard3);
 
         Vector3 tcpos4 = new Vector3(6f, 1.9f, 0);
-        GameObject TreasureCard4 = treasureCards[Random.Range(0, 21)];
+        GameObject TreasureCard4 = gameobjectsScript.treasureCards[Random.Range(0, 21)];
         TreasureCard4.transform.position = tcpos4;
-        treasureCards.Remove(TreasureCard4);
+        gameobjectsScript.treasureCards.Remove(TreasureCard4);
     }
 
     public void AddWaterRiseCardsToDeck()
     {
-        treasureCards.Add(GameObject.Find("Water Rise 1"));
-        treasureCards.Add(GameObject.Find("Water Rise 2"));
-        treasureCards.Add(GameObject.Find("Water Rise 3"));
+        gameobjectsScript.treasureCards.Add(GameObject.Find("Water Rise 1"));
+        gameobjectsScript.treasureCards.Add(GameObject.Find("Water Rise 2"));
+        gameobjectsScript.treasureCards.Add(GameObject.Find("Water Rise 3"));
     }
 
     public void AdventurerButtonDissapears()
@@ -78,5 +86,20 @@ public class Setup : GameObjects
         treasureCardButton = GameObject.Find("Treasure Cards");
 
         treasureCardButton.SetActive(false);
+    }
+
+    public void TilesFlipInitial()
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            gameobjectsScript.tilesonBoard[Random.Range(0, 23)].GetComponent<Renderer>().material.color = Color.cyan;
+        }
+    }
+
+    public void InitialFloodCardButtonDissapears()
+    {
+        initialFloodCardButton = GameObject.Find("Initial Flood Cards");
+
+        initialFloodCardButton.SetActive(false);
     }
 }
---
+++ Assets/Scripts/WaterRise.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterRise : MonoBehaviour
+{
+    public int WaterRiseNumber;
+
+    //Need to add something to water rise cards, that if its picked up, it adds to list
+
+    public void WaterRiseFloodCards()
+    {
+        //if (Water rise list contains 1 or 2 cards)
+        //{
+        //pick up 2 flood cards on player turn
+        //}
+        //else if (Water rise list contains 3, 4, 5 cards)
+        //{
+        //pick up 3 flood cards on player turn
+        //}
+        //else if (Water rise list contains 6, 7 cards)
+        //{
+        //pick up 4 flood cards on player turn
+        //}
+        //else if (Water rise list contains 8, 9 cards)
+        //{
+        //pick up 5 flood cards on player turn
+        //}
+        //else if (Water rise list contains 10 cards)
+        //{
+        //Lose the game
+        //}
+    }
+}
